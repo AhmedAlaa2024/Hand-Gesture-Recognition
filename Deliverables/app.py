b0212426f4939c42aab8bf3ad5cf9ffae7f90c85
zeinabmoawad <92188433+zeinabmoawad@users.noreply.github.com>
2023-05-16 20:47:15 +0300

    semi semi semi final

--- Deliverables/app.py
+++ Deliverables/app.py
@@ -6,17 +6,17 @@ import numpy as np
 import time
 # Stage 6: Model Inference
 # Load the model
-with open('models/hard_training_voting.hdf5', 'rb') as file:
+with open('models/soft_training_voting.hdf5', 'rb') as file:
   soft_voting = joblib.load(file)
     # Initialize an empty list to store the numbers
 # Load the images
-directory = "test"
+directory = "data"
 image_paths = []
 for filename in os.listdir(directory):
   if filename.endswith('.jpg') or filename.endswith('.png'):
     image_paths.append(os.path.join(directory, filename))
 if not image_paths:
-  print('No images found in the test directory')
+  print('No images found in the data directory')
   exit()
 
 # Make predictions and record times
@@ -25,11 +25,11 @@ times = []
 resizedImage = []
 for image_path in image_paths:
   image = cv2.imread(image_path)
+  start_time = time.time()
   image = hog_preprocessing(image)
   resizedImage.append(image)
   hog_feature_vector = extract_hog_features(image, np.array([10, 10]), 9)
   hog_feature_vector = np.asarray(hog_feature_vector).reshape(1, -1)
-  start_time = time.time()
   soft_prediction = soft_voting.predict(hog_feature_vector)
   end_time = time.time()
   soft_predictions.append(soft_prediction[0])
